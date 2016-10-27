4c8d0fae255867447a288c9f29d1d8ee518e2d5b
Al Ilseman <al@ilseman.com>
2016-10-27 16:42:24 -0500

    testing Users.all and passing too

--- app/services/users/users.js
+++ app/services/users/users.js
@@ -2,9 +2,14 @@
 	'use strict';
 
 	// Create the module and factory we referenced in beore_each blocks
+
 	angular.module('api.users', [])
 	.factory('Users', function() {
+
 		var Users = {};
+
+		Users.all = function() {};
+
 		return Users;
 	});
 })();
--- app/services/users/users.spec.js
+++ app/services/users/users.spec.js
@@ -17,4 +17,11 @@ describe('Users factory', function() {
 	it('has a dummy spec to test math', function() {
 		expect(2 + 3).toEqual(5);
 	});
+
+	describe('.all()', function() {
+		// verify that the .all() method exists
+		it('should exist', function() {
+			expect(Users.all).toBeDefined();
+		});
+	});
 });
