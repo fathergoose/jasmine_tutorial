468c2423bb0b814df3a965de34b772589e3aaaa0
Al Ilseman <al@ilseman.com>
2016-10-27 16:09:28 -0500

    tests are poperly failing now

--- app/services/users/users.spec.js
+++ app/services/users/users.spec.js
@@ -1,4 +1,18 @@
 describe('Users factory', function() {
+	var Users;
+
+	// before each test, load the api.users module
+	beforeEach(angular.mock.module('api.users'));
+
+	// before each test, set our injected users factory (_Users_) to our local Users
+	beforeEach(inject(function(_Users_) {
+		Users = _Users_;
+	}));
+
+	// a simple test to verify the Users factory exists
+	it('should be defined', function() {
+		expect('Users').toBeDefined();
+	});
 
 	it('has a dummy spec to test math', function() {
 		expect(2 + 3).toEqual(5);
