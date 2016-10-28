afb47e2fb3be4492d86532d25958270beed4e209
Al Ilseman <al@ilseman.com>
2016-10-28 10:50:22 -0500

    userList and Users.all(); is working/passing

--- app/services/users/users.spec.js
+++ app/services/users/users.spec.js
@@ -19,9 +19,43 @@ describe('Users factory', function() {
 	});
 
 	describe('.all()', function() {
-		// verify that the .all() method exists
-		it('should exist', function() {
+		var userList = [
+			{
+	        id: '1',
+	        name: 'Jane',
+	        role: 'Designer',
+	        location: 'New York',
+	        twitter: 'gijane'
+	      },
+	      {
+	        id: '2',
+	        name: 'Bob',
+	        role: 'Developer',
+	        location: 'New York',
+	        twitter: 'billybob'
+	      },
+	      {
+	        id: '3',
+	        name: 'Jim',
+	        role: 'Developer',
+	        location: 'Chicago',
+	        twitter: 'jimbo'
+	      },
+	      {
+	        id: '4',
+	        name: 'Bill',
+	        role: 'Designer',
+	        location: 'LA',
+	        twitter: 'dabill'
+	      }
+      ]
+
+		it('should be defined', function() {
 			expect(Users.all).toBeDefined();
+		});
+
+		it('should return a hard-coded list of users', function() {
+			expect(Users.all()).toEqual(userList);
 		});
 	});
 });
